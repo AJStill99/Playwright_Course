78e1fa149543e3a85fd2bb908db36560ba32f351
Alex Still <alexstill@hotmail.co.uk>
2025-11-12 00:43:25 +0000

    Removing unused code

--- tests/UI_Basics.spec.js
+++ tests/UI_Basics.spec.js
@@ -1,32 +1,4 @@
 const {test, expect} = require('@playwright/test');
-const { todo } = require('node:test');
-
-test('First Test', async ({browser}) => {
-    // Instead of page or browser, can pass nothing and be treated as an anon function
-    // the await keyword will execute the command and wait for it to finish
-    // Need to declare async before using await
-
-    // const context = await browser.newContext();
-
-    // ^Start a new instance with fixed properties within the brackets bit of this function
-    // For exmaple, can control the cookie settings, geolocation (proxy), permissions, etc.
-
-    // const page = await context.newPage();
-
-    // ^Opens a new tab in the browser context
-
-    /* The above block of code will be done automatically using the page fixture, 
-    so need for it here */
-
-    // await page.goto('https://www.example.com');
-
-    // ^Navigates to the URL
-
-    // Creates a new context, a new page, and goes to the URL
-
-});
-
-// easier way to write this below:
 
 test('Browser context test', async ({page}) => {
     // This achieves the same as above, but far cleaner
