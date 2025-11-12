afb56769bc879b3ed36b216baf9d5af9780de007
Alex Still <alexstill@hotmail.co.uk>
2025-11-12 23:44:01 +0000

    Removing redundant comments

--- tests/UI_Basics.spec.js
+++ tests/UI_Basics.spec.js
@@ -1,33 +1,19 @@
 const {test, expect} = require('@playwright/test');
 
 test('Browser context test', async ({page}) => {
-    // This achieves the same as above, but far cleaner
     await page.goto('https://www.example.com');
     console.log("Running test...");
-
-    //Get title, and put assertion if correct
     console.log(await page.title());
     await expect(page).toHaveTitle("Example Domain");
-    // Assertion to check title is correct
 });
 
 test('UI Test', async ({page}) => {
     await page.goto('https://google.com');
-
     await page.reload();
     // Check for any refresh issues 
-
     const currentUrl = await page.url();
     console.log(currentUrl);
 });
-
-// Can run using the --headed flag to see the browser UI
-
-// test.only("UI Basics, only running this test in a batch command", async ({page}) => {
-//     console.log("UI Basics test started");
-// });
-
-// When run, only the above test will run in the batch command
 
 test("Page playwright tests", async ({page}) => {
     await page.goto("https://google.com");
@@ -36,55 +22,36 @@ test("Page playwright tests", async ({page}) => {
     // Makes it clear which browser is being used
 
     await expect(page).toHaveTitle("Google");
-
-
 });
 
 test("CSS Selectors and Playwright locators", async ({page}) => {
+    // Initialize
     const userName = await page.locator('#username');
     const signIn = await page.locator('#signInBtn');
     const cardTitles = await page.locator('.card-body a')
+    // STEP - Go to URL
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
 
-    // CSS selectors
+    // SECTION - CSS selectors
     await userName.fill("rahulshetty");
     await page.locator("[type='password']").fill("learning");
     await signIn.click();
-    // Can use type, or fill for the above code block but fill is better now
-    // Notice we are refactoring code here
-
     // This will produce an error as the username is incorrect
     // Will need to handle the error message popup
 
     await page.locator("[style*='block']");
     // Partial match using wildcard * for style attribute
     // Don't need to explicitly wait to load, as plywright does this automatically
-
     console.log(await page.locator("[style*='block']").textContent());
-    // Logs the error message text
-
     await expect(page.locator("[style*='block']")).toContainText('Incorrect');
-
-    // await expect(page.locator("[style*='block']")).toContainText('Incorrectt');
-    // Assertion to check error message contains 'Incorrect'
-    // The above will fail due to the typo in the text, but display how the expect timeout works really well, commented out to avoid a test failure
-    await userName.fill("");
-    // will fill an emtpy string
-
-    // Correct username and password
+    await userName.fill(""); // will fill an emtpy string
     await userName.fill("rahulshettyacademy");
     await signIn.click();
     await page.locator("[type='password']").fill("learning");
 
-    console.log(await cardTitles.first().textContent());
-    // This will return the first element of the selectors if multiple
-    // Can achieve a similar affect by using .nth(0)
-    // The above could be used in a for loop maybe? 
-
+    console.log(await cardTitles.first().textContent()); // Can achieve a similar affect by using .nth(0)
     const allTitles = await cardTitles.allTextContents();
-    // Not waiting until contents are present
-    // Returns all elements
     // Will pass in this instance because Playwright will wait for .textContent assertion, but if this is commented out, it will return nothing
     console.log(allTitles);  
 });
