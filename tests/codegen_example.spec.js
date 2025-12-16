67e17f508aa023ad4843dc1d19dd59ba455604ba
Alex Still <alexstill@hotmail.co.uk>
2025-12-16 15:31:16 +0000

    Codegen examp;e using google

--- tests/codegen_example.spec.js
+++ tests/codegen_example.spec.js
@@ -2,6 +2,24 @@ const {test, expect} = require('@playwright/test');
 
 test.describe("Using the codegen tool", () => {
     test("First example test", async ({page}) => {
-        
+        await page.goto('https://www.google.com/');
+        await page.getByRole('button', { name: 'Accept all' }).click();
+        await page.getByRole('combobox', { name: 'Search' }).click();
+        await page.getByRole('combobox', { name: 'Search' }).fill('This is a test');
+        await page.locator('iframe[name="a-hvxeozm9v6ob"]').contentFrame().getByRole('checkbox', { name: 'I\'m not a robot' }).click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="0"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="2"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="5"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().getByRole('button', { name: 'Verify' }).click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="13"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="14"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="9"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().getByRole('button', { name: 'Next' }).click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="9"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="8"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="4"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().locator('[id="5"]').click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().getByRole('button', { name: 'Verify' }).click();
+        await page.locator('iframe[name="c-hvxeozm9v6ob"]').contentFrame().getByRole('button', { name: 'Verify' }).click();
     });
 });
